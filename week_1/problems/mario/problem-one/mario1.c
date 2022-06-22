d9d7c32748546d5a842a858f466a379c309bce88
fabriciobgrigolo <fbc.grigolo@gmail.com>
2022-06-22 20:56:45 -0300

    first mario problem resolution

---
+++ week_1/problems/mario/problem-one/mario1.c
@@ -0,0 +1,62 @@
+// This is the resolution for the problem set one of two
+// https://cs50.harvard.edu/x/2022/psets/1/mario/less/
+
+
+#include <cs50.h>
+#include <stdio.h>
+
+void bricks(int n);
+void dots(int n, int n2);
+
+int main(void)
+{
+    // Declaring int variable
+    int h;
+    do
+    {
+        // Ask this question while h is less than one or greater than eight
+        printf("Choose a number between one and eight for the size of the right triangle.\n");
+        h = get_int("Size: ");
+    }
+    while (h < 1 || h > 8);
+
+    // for each row
+    for (int i = 0; i < h; i++)
+    {
+        dots(i, h), bricks(i);
+        printf("\n");
+
+
+    }
+
+}
+
+// Functions are below ↓
+//                     ↓
+//                     ↓
+//                     ↓
+
+
+
+
+
+
+
+
+
+
+void bricks(int n)
+{
+    for (int i = -1; i < n; i++)
+    {
+        printf("#");
+    }
+}
+
+void dots(int n, int n2)
+{
+    for (int i = n2 - 1; i > n; i--)
+    {
+        printf(" ");
+    }
+}
